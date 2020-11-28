268eb4b01e3e1697a5171520db5476554f536f00
SREEHARI <sreehari2610@gmail.com>
2020-11-28 14:49:10 +0530

    input and button-initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+const[input,setInput]=useState('');
+const[messages,setMessage]=useState([])
+
+const SendMessage=(event)=>{
+  event.preventDefault();
+  setMessage([...messages,input])
+  setInput('')
+
+}
+console.log(messages);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <form>
+
+      <input value={input} onChange={e=>setInput(e.target.value)}/>
+      <button  type='submit' onClick={SendMessage}>Send Message</button>
+      </form>
+   
+    {
+      messages.map(message=>(<p>{message}</p>))
+    }
     </div>
   );
 }
